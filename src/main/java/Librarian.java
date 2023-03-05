c75cabb58d858653b728fe7bd218680dbe955e4f
mobina kargar <dymamsijhidjj@gmail.com>
2023-03-05 21:05:25 +0330

    start

--- src/main/java/Librarian.java
+++ src/main/java/Librarian.java
@@ -2,9 +2,37 @@ public class Librarian {
     /*
     * The librarian should have a username and a password
     * The librarian should be able to search users, librarians and books
-    * The librarian should be able to add\remove\update user add\remove\update_
-    _ librarian and add\remove\update book
+    * The librarian should be able to add\remove/update user add\remove/update_
+    _ librarian and add\remove/update book
      */
+    private String username;
+    private String password;
+    public Librarian(){
 
+        username = "Comody";
+        password ="LibraryGZ2004";
 
+    }
+    public Librarian(String username , String password){
+
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
