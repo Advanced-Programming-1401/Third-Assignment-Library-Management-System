328e1a90c38022885b4ae9473e6e344e35f1ae45
mobina kargar <dymamsijhidjj@gmail.com>
2023-03-05 22:26:35 +0330

    second

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,3 +1,30 @@
 public class Book {
     //Book should contain name,author,year of publish and ISBN
+       private int ISBN , YearPublish;
+       private String name,author;
+       public Book(){
+           name ="";
+           author="";
+           YearPublish = 0;
+           ISBN = 0;
+
+       }
+       public Book(String name,String author,int ISBN,int YearPublish){
+
+           this.name = name;
+           this.author = author;
+           this.ISBN = ISBN;
+           this.YearPublish = YearPublish;
+
+       }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "ISBN=" + ISBN +
+                ", YearPublish=" + YearPublish +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
