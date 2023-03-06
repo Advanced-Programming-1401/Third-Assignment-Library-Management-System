1aa38afa2448e886491ff3baf1b91bd1cd431120
mobina kargar <dymamsijhidjj@gmail.com>
2023-03-06 17:03:34 +0330

    third

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList;
+
 public class Book {
     //Book should contain name,author,year of publish and ISBN
-       private int ISBN , YearPublish;
+
+       private int YearPublish, ISBN ;
        private String name,author;
        public Book(){
            name ="";
            author="";
            YearPublish = 0;
            ISBN = 0;
+/*
+           ArrayList<Book> Books = new ArrayList<>();
+           Book book1 = new Book("Maze Runner","James Dasher",13,2009);
+           Book book2 = new Book("Hunger Games" , "Suzanne Collins" ,10,2013);
+           Book book3 = new Book("Elon Musk","Ashlee Vance",8,2018);
+           Book book4 = new Book("It Ends With Us", "Colleen Hoover" ,4,2018);
+           Books.add(book1);
+           Books.add(book2);
+           Books.add(book3);
+           Books.add(book4);*/
 
        }
        public Book(String name,String author,int ISBN,int YearPublish){
@@ -17,6 +30,22 @@ public class Book {
            this.YearPublish = YearPublish;
 
        }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public int getYearPublish() {
+        return YearPublish;
+    }
 
     @Override
     public String toString() {
--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,3 +1,7 @@
+import java.awt.print.Book;
+import java.util.*;
+import java.util.HashMap;
+
 public class Library {
     /*
     * The library should have a list of books.
@@ -5,78 +9,94 @@ public class Library {
     -> (for example: harry potter -> 4 means there are currently 4 harry potter books)
     * The library should have a list of users and a list of librarians.
      */
+    private ArrayList collection;
+    //ArrayList<String> collection= new ArrayList<Book>();
+
+    public Library(){
+        collection = new ArrayList<Book>();
+    }
+
+/*
+    HashMap<String, Book> directory = new HashMap<>();
+      directory.put(book1.getName(), book1);
+      directory.put(book2.getName(), book2);
+*/
 
     //book related functions
+    public void addBook(Book book){
+        //TODO
+        collection.add(book);
+    }
 
-    public void addBook(){
+
+    public void removeBook(Book book){
+        //TODO
+        collection.remove(book);
+    }
+
+    public void searchBook(Book book){
+        //TODO
+
+
+    }
+
+    public void updateBook(Book book){
         //TODO
     }
 
-    public void removeBook(){
+    public void doesBookExist(Book book){
         //TODO
     }
 
-    public void searchBook(){
+    public void increaseBook(Book book){
         //TODO
     }
 
-    public void updateBook(){
-        //TODO
-    }
-
-    public void doesBookExist(){
-        //TODO
-    }
-
-    public void increaseBook(){
-        //TODO
-    }
-
-    public void decreaseBook(){
+    public void decreaseBook(Book book){
         //TODO
     }
 
     //user related functions
 
-    public void addUser(){
+    public void addUser(User add){
         //TODO
     }
 
-    public void removeUser(){
+    public void removeUser(User remove){
         //TODO
     }
 
-    public void searchUser(){
+    public void searchUser(User search){
         //TODO
     }
 
-    public void updateUser(){
+    public void updateUser(User update){
         //TODO
     }
 
-    public void doesUserExist(){
+    public void doesUserExist(User exist ){
         //TODO
     }
 
     //librarian related functions
 
-    public void addLibrarian(){
+    public void addLibrarian(Librarian add){
         //TODO
     }
 
-    public void removeLibrarian(){
+    public void removeLibrarian(Librarian remove){
         //TODO
     }
 
-    public void searchLibrarian(){
+    public void searchLibrarian(Librarian search){
         //TODO
     }
 
-    public void updateLibrarian(){
+    public void updateLibrarian(Librarian update){
         //TODO
     }
 
-    public void doesLibrarianExist(){
+    public void doesLibrarianExist(Librarian exist){
         //TODO
     }
 
