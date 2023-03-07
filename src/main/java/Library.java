a2f153368fe9f2aad1689c1f39b5f60a90361831
mobina kargar <dymamsijhidjj@gmail.com>
2023-03-07 12:29:06 +0330

    Confirm Library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -11,6 +11,7 @@ public class Library  extends Book{
      */
     static Scanner input = new Scanner(System.in);
     private ArrayList <Book> BookList= new ArrayList<Book>();
+     //private HashMap<User, String  > UserList = new HashMap<String, String>();
     private ArrayList <User> UserList = new ArrayList<User>();
     private ArrayList <Librarian> LibrarianList = new ArrayList<Librarian>();
     String booksearch;
@@ -30,13 +31,11 @@ public class Library  extends Book{
 
     //book related functions
     public void addBook(Book book){
-        //TODO
         BookList.add(book);
     }
 
 
     public void removeBook(int ISBN){
-        //TODO
 
         boolean successful = false;
         for (int i=0;i<BookList.size();i++)
@@ -53,10 +52,10 @@ public class Library  extends Book{
         {
             System.out.println("Could not remove book "+ ISBN );
         }
+
     }
 
     public Book searchBook(){
-        //TODO
 
         System.out.println("Which book are you searching for ?");
         booksearch = input.nextLine();
@@ -72,17 +71,27 @@ public class Library  extends Book{
     }
 
     public void updateBook(String UpdateName , String UpdateAuthor ,int UpdateYear , int UpdateISBN){
-        //TODO
+
         Book book = null;
         book.setName(UpdateName);
         book.setAuthor(UpdateAuthor);
         book.setYearPublish(UpdateYear);
         book.setISBN(UpdateISBN);
+
     }
 
-    public void doesBookExist(Book book){
-        //TODO
+    public void doesBookExist(){
 
+        System.out.println("Know about your seaerching book to exist or not = ");
+        booksearch = input.nextLine();
+        for (Book book : BookList)
+        {
+            if (book.getName().contains(booksearch) || book.getAuthor().contains(booksearch))
+            {
+               System.out.println("NOT EXIST");
+            }
+        }
+        System.out.println("EXIST"); //No book was found
 
     }
 
@@ -94,49 +103,134 @@ public class Library  extends Book{
         //TODO
     }
 
+//................................................................
+
     //user related functions
 
-    public void addUser(User add){
-        //TODO
+    public void addUser(User username , User password){
+
+         UserList.add(username);
+         UserList.add(password);
+        System.out.println("Welcome!  " + username );
+
     }
 
-    public void removeUser(User remove){
-        //TODO
+    public void removeUser(String username ,String password){
+
+        boolean successful = false;
+        for (int i = 0 ; i < UserList.size() ; i++)
+        {
+            if(UserList.equals(UserList.get(i).getUsername()))
+            {
+                UserList.remove(username);
+                UserList.remove(password);
+
+                System.out.println("User removal successful");
+                successful=true;
+            }
+        }
+
+        if (!successful)
+        {
+            System.out.println("There is no user with "+ username +"username and "+password+" this password" );
+        }
     }
 
-    public void searchUser(User search){
-        //TODO
+    public User searchUser(String Usersearch){
+
+        System.out.println("Which User are you searching for ?");
+        Usersearch = input.nextLine();
+        for (User user : UserList)
+        {
+            if (user.getUsername().contains(booksearch))
+            {
+                return user;
+            }
+        }
+        return null; //No user was found
     }
 
-    public void updateUser(User update){
-        //TODO
+    public void updateUser(String username , String password ){
+        User user = null;
+        user.setUsername(username);
+        user.setPassword(password);
+
     }
 
-    public void doesUserExist(User exist ){
-        //TODO
+    public void doesUserExist(String Usersearch ){
+        System.out.println("Which User are you searching for ?");
+        Usersearch = input.nextLine();
+        for (User user : UserList)
+        {
+            if (user.getUsername().contains(Usersearch))
+            {
+               System.out.println("EXIST");
+            }
+        }
+        System.out.println("NO User EXIST"); //No user was found
     }
 
     //librarian related functions
 
-    public void addLibrarian(Librarian add){
-        //TODO
+    public void addLibrarian(Librarian username , Librarian password){
+        LibrarianList.add(username);
+        LibrarianList.add(password);
+        System.out.println("Welcome!  MRS/MS " + username );
+
     }
 
-    public void removeLibrarian(Librarian remove){
-        //TODO
+    public void removeLibrarian(String username , String password){
+
+        boolean successful = false;
+        for (int i = 0 ; i < LibrarianList.size() ; i++)
+        {
+            if(LibrarianList.equals(LibrarianList.get(i).getUsername()))
+            {
+                LibrarianList.remove(username);
+                LibrarianList.remove(password);
+
+                System.out.println("Librarian removal successful");
+                successful=true;
+            }
+        }
+
+        if (!successful)
+        {
+            System.out.println("There is no librarian with "+ username +"username and "+password+" this password" );
+        }
     }
 
-    public void searchLibrarian(Librarian search){
-        //TODO
+    public Librarian searchLibrarian(String  Librariansearch){
+        System.out.println("Which Librarian are you searching for ?");
+        Librariansearch = input.nextLine();
+        for (Librarian librarian : LibrarianList)
+        {
+            if (librarian.getUsername().contains(booksearch))
+            {
+                return librarian;
+            }
+        }
+        return null; //No user was found
     }
 
-    public void updateLibrarian(Librarian update){
-        //TODO
+    public void updateLibrarian(String username , String password){
+        User user = null;
+        user.setUsername(username);
+        user.setPassword(password);
     }
 
-    public void doesLibrarianExist(Librarian exist){
-        //TODO
+    public void doesLibrarianExist(String LibrarianSearch){
+        System.out.println("Know about your seaerching librarian to exist or not = ");
+         LibrarianSearch = input.nextLine();
+        for (Librarian librarian : LibrarianList)
+        {
+            if (librarian.getUsername().contains(LibrarianSearch))
+            {
+                System.out.println("EXIST");
+            }
+        }
+        System.out.println("NO User EXIST"); //No user was found
     }
 
+    }
 
-}
